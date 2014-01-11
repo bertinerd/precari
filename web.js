2518c33a9231fb6fe7e00d74153020b3a837f5dc
Luca Bertinetto <luca.bertinetto@gmail.com>
2014-01-11 17:48:21 +0100

    Change to precari.herokuapp

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Ok. This is my second edit to Heroku, don\'t take it seriously.');
+  response.send('Changed to precari.herokuapp');
 });
 
 var port = process.env.PORT || 5000;
