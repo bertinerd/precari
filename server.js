5d84f6aeed4dc5bb100d67258bae430edd0606ce
Luca Bertinetto <luca.bertinetto@gmail.com>
2014-01-11 19:08:50 +0100

    .

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(request, response) {
 	res.render('index.html');
-	response.send('Changed to precari.herokuapp');
+	// response.send('Changed to precari');
 });
 
 var port = process.env.PORT || 5000;
